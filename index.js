15e361233a7cdd7691b37b7fe36beb72067d2a03
Takashi <th2826@columbia.edu>
2021-08-15 13:21:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,64 +1,37 @@
-// Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-//.push (add at the end)
 function destructivelyAppendCat(name){
-    cats.push("Ralph");
+    cats.push(name);
 }
 
-//.upshift (add at the begging)
 function destructivelyPrependCat(name){
-    cats.unshift("Bob");
+    cats.unshift(name);
 }
 
-//.shift (remove at the begging)
-function destructivelyRemoveLastCat(name){
+function destructivelyRemoveLastCat(){
     cats.pop();
 }
 
-//.shift (remove at the begging)
-function destructivelyRemoveFirstCat(name){
+function destructivelyRemoveFirstCat(){
     cats.shift();
 }
 
-//Spread Operator (make a copy and add at the end)
-function appendCat(name) {
-    const newCatsArray = [...cats, 'Broom']; 
-    return newCatsArray;
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
 }
 
-//Spread Operator (make a copy and add at the beggining)
 function prependCat(name){
-    const newCatsArray = ["Arnold", ... cats]
-    return newCatsArray;
+    const newCats = [name, ...cats];
+    return newCats;
 }
 
-/*
-Alternative solutions
-//Spread Operator (make a copy and add at the end)
-function appendCat(name) {
-    const newCatsArray = [...cats, name]; 
-    return newCatsArray;
-}
-appendCat('Broom');
-
-//Spread Operator (make a copy and add at the beggining)
-function prependCat(name){
-    const newCatsArray = [name, ... cats]
-    return newCatsArray;
-}
-prependCat("Arnold")
-*/
-
-// (make a copy and remove the last one)
-function removeLastCat(name){
-    const copyOfCats = cats.slice(0, cats.length -1);
-    return copyOfCats;
+function removeLastCat(){
+    const newCats = cats.slice(0, 2);
+    return newCats;
 }
 
-// (make a copy and remove the first one)
-function removeFirstCat(name){
-    const copyOfCats = cats.slice(1);
-    return copyOfCats;
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
 }
-
